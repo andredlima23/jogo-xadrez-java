34d210035cc7803780e9b4edb3ed55c9f41780fd
André Lima <andredlima23@gmail.com>
2020-10-02 13:52:01 -0300

    criação da classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Adeus, mundo!");
+		Position p = new Position(3, 5);
+		System.out.println(p);
 	}
 
 }
