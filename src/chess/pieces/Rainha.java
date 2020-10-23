9a7754b8750f2574c44d2b8a1c3acd3a6faa601f
André Lima <andredlima23@gmail.com>
2020-10-23 15:34:08 -0300

    Classe Cavalo anteriormente e classe rainha

---
+++ src/chess/pieces/Rainha.java
@@ -0,0 +1,107 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rainha extends ChessPiece {
+
+	public Rainha(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+
+		Position p = new Position(0, 0);
+
+		// Posições acima da peça
+		p.setValues(position.getLinha() - 1, position.getColuna());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Posições a esquerda da peça
+		p.setValues(position.getLinha(), position.getColuna() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Posições a direita da peça
+		p.setValues(position.getLinha(), position.getColuna() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Posições abaixo da peça
+		p.setValues(position.getLinha() + 1, position.getColuna());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		// Posições a noroeste da peça
+		p.setValues(position.getLinha() - 1, position.getColuna() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Posições a nordeste da peça
+		p.setValues(position.getLinha() - 1, position.getColuna() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Posições a sudeste da peça
+		p.setValues(position.getLinha() + 1, position.getColuna() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Posições sudoeste da peça
+		p.setValues(position.getLinha() + 1, position.getColuna() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		return mat;
+	}
+
+}
