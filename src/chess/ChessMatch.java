3b1b92e24ac51c777921e0e88fd05e17424c5e7d
André Lima <andredlima23@gmail.com>
2020-10-23 16:11:31 -0300

    Movimento especial Roque

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -98,6 +98,24 @@ public class ChessMatch {
 			capturedPieces.add(capturedPiece);
 		}
 
+		// #Movimento especial Roque Pequeno
+		if (p instanceof Rei && target.getColuna() == source.getColuna() + 2) {
+			Position sourceT = new Position(source.getLinha(), source.getColuna() + 3);
+			Position targetT = new Position(source.getLinha(), source.getColuna() + 1);
+			ChessPiece torre = (ChessPiece) board.removePiece(sourceT);
+			board.placePiece(torre, targetT);
+			torre.increaseMoveCount();
+		}
+
+		// #Movimento especial Roque Grande
+		if (p instanceof Rei && target.getColuna() == source.getColuna() - 2) {
+			Position sourceT = new Position(source.getLinha(), source.getColuna() - 4);
+			Position targetT = new Position(source.getLinha(), source.getColuna() - 1);
+			ChessPiece torre = (ChessPiece) board.removePiece(sourceT);
+			board.placePiece(torre, targetT);
+			torre.increaseMoveCount();
+		}
+
 		return capturedPiece;
 	}
 
@@ -110,6 +128,24 @@ public class ChessMatch {
 			board.placePiece(capturedPiece, target);
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
+		}
+
+		// #Desfazendo movimento especial Roque Pequeno
+		if (p instanceof Rei && target.getColuna() == source.getColuna() + 2) {
+			Position sourceT = new Position(source.getLinha(), source.getColuna() + 3);
+			Position targetT = new Position(source.getLinha(), source.getColuna() + 1);
+			ChessPiece torre = (ChessPiece) board.removePiece(targetT);
+			board.placePiece(torre, sourceT);
+			torre.decreaseMoveCount();
+		}
+
+		// #Desfazendo movimento especial Roque Grande
+		if (p instanceof Rei && target.getColuna() == source.getColuna() - 2) {
+			Position sourceT = new Position(source.getLinha(), source.getColuna() - 4);
+			Position targetT = new Position(source.getLinha(), source.getColuna() - 1);
+			ChessPiece torre = (ChessPiece) board.removePiece(targetT);
+			board.placePiece(torre, sourceT);
+			torre.decreaseMoveCount();
 		}
 	}
 
@@ -202,7 +238,7 @@ public class ChessMatch {
 		placeNewPiece('b', 1, new Cavalo(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bispo(board, Color.WHITE));
 		placeNewPiece('d', 1, new Rainha(board, Color.WHITE));
-		placeNewPiece('e', 1, new Rei(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rei(board, Color.WHITE, this));
 		placeNewPiece('f', 1, new Bispo(board, Color.WHITE));
 		placeNewPiece('g', 1, new Cavalo(board, Color.WHITE));
 		placeNewPiece('h', 1, new Torre(board, Color.WHITE));
@@ -219,7 +255,7 @@ public class ChessMatch {
 		placeNewPiece('b', 8, new Cavalo(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bispo(board, Color.BLACK));
 		placeNewPiece('d', 8, new Rainha(board, Color.BLACK));
-		placeNewPiece('e', 8, new Rei(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rei(board, Color.BLACK, this));
 		placeNewPiece('f', 8, new Bispo(board, Color.BLACK));
 		placeNewPiece('g', 8, new Cavalo(board, Color.BLACK));
 		placeNewPiece('h', 8, new Torre(board, Color.BLACK));
--- src/chess/pieces/Rei.java
+++ src/chess/pieces/Rei.java
@@ -2,80 +2,112 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Rei extends ChessPiece {
 
-	public Rei(Board board, Color color) {
+	private ChessMatch chessMatch;
+
+	public Rei(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
-		
+		this.chessMatch = chessMatch;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
 	}
-	
+
 	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
+	}
+
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p instanceof Torre && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
-		
+
 		Position p = new Position(0, 0);
-		
+
 		// Posições possíveis acima do rei
 		p.setValues(position.getLinha() - 1, position.getColuna());
 		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis abaixo do rei
 		p.setValues(position.getLinha() + 1, position.getColuna());
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis a esquerda do rei
 		p.setValues(position.getLinha(), position.getColuna() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis a direita do rei
 		p.setValues(position.getLinha(), position.getColuna() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis a noroeste do rei
 		p.setValues(position.getLinha() - 1, position.getColuna() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis a nordeste do rei
 		p.setValues(position.getLinha() - 1, position.getColuna() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis a sudoeste do rei
 		p.setValues(position.getLinha() + 1, position.getColuna() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// Posições possíveis a sudeste do rei
 		p.setValues(position.getLinha() + 1, position.getColuna() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
-		mat[p.getLinha()][p.getColuna()] = true;	
+			mat[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
+		// #Movimento especial Roque
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// #Movimento especial Roque Pequeno
+			Position posT1 = new Position(position.getLinha(), position.getColuna() + 3);
+			if (testRookCastling(posT1)) {
+				Position p1 = new Position(position.getLinha(), position.getColuna() + 1);
+				Position p2 = new Position(position.getLinha(), position.getColuna() + 2);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					mat[position.getLinha()][position.getColuna() + 2] = true;
+				}
+			}
+
+			// #Movimento especial Roque Grande
+			Position posT2 = new Position(position.getLinha(), position.getColuna() - 4);
+			if (testRookCastling(posT2)) {
+				Position p1 = new Position(position.getLinha(), position.getColuna() - 1);
+				Position p2 = new Position(position.getLinha(), position.getColuna() - 2);
+				Position p3 = new Position(position.getLinha(), position.getColuna() - 3);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					mat[position.getLinha()][position.getColuna() - 2] = true;
+				}
+			}
+		}
+
 		return mat;
 	}
 
